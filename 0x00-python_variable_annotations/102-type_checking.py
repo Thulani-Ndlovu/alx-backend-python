5eed728d8f96c9c3afcf364f17236a752523580d
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-01-12 22:18:59 +0200

    Task 12. Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Validating code using mypy'''
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    '''
+        Uses mypy to validate the following piece
+        of code and apply any necessary changes.
+    '''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
