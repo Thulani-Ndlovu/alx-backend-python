5ead351f009a413b7dbb084070337d2b856b4385
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-15 21:36:33 +0200

    Task 3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -60,3 +60,25 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**attributes)) as getReq:
             self.assertEqual(get_json(test_url), test_payload)
             getReq.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    '''Test Memoization Functionality'''
+    def test_memoize(self) -> None:
+        '''Test memoize'''
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memoFun:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memoFun.assert_called_once()
