b750d866a0220a61d9ae4785a0fe827de0e25c4f
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-15 21:53:10 +0200

    Task 5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -33,3 +33,17 @@ class TestGithubOrgClient(unittest.TestCase):
         mockedFun.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
+
+    def test_public_repos_url(self) -> None:
+        '''Test public repos url'''
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
