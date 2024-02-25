940a16a75a1a5ed613f0d3fc1767a5870f58cc41
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-25 20:21:03 +0200

    Task 9. Integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -142,6 +142,20 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
 
+    def test_public_repos(self) -> None:
+        '''tests public repos'''
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_licenses(self) -> None:
+        '''test public repos with licenses'''
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
+
     @classmethod
     def tearDownClass(cls) -> None:
         '''tear down class'''
